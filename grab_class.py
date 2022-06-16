559dbf03495d2f1780c387bb551ded2bc505a4ad
sayampalrecha <sayampalrecha09@gmail.com>
2022-06-16 19:12:19 +0530

    class

---
+++ grab_class.py
@@ -0,0 +1,13 @@
+#  this iss the code to easily scrap out all the information or data from a website!!
+#  Importing the library
+import requests
+import bs4
+
+# getting the URL
+result  = requests.get("https://en.wikipedia.org/wiki/Narendra_Modi")
+
+soup = bs4.BeautifulSoup(result.text,"lxml")
+
+
+for item in soup.select('.toctext'):
+    print(item.text)
